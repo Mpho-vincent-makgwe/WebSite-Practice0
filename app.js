270e2c4fa83fc91e4e30b8cb567fe18eb06d10ce
Mpho Vincent Makgwe <mphomakgwe4@gmail.com>
2023-02-23 10:43:11 +0200

    yah neh

--- app.js
+++
@@ -1,7 +0,0 @@
-const COLORS = [
-{r: 45, g: 74, b: 227} // blue
-{r: 250, g: 255, b: 89} //yellow
-{r: 45, g: 74, b: 227} //pupple
-{r: 45, g: 74, b: 227} //skyblue
-{r: 45, g: 74, b: 227} //green
-]
